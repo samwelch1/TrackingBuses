3eb2cfd727d1acf8d2f6f02fff5e857a0b81e858
Sam Welch <shwelch@stu.naperville203.org>
2018-02-07 12:37:09 -0600

    create xcode project
    maybe

---
+++ Tracking Buses SE/Tracking Buses SE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tracking Buses SE
+//
+//  Created by ncprg on 2/7/18.
+//  Copyright © 2018 ncprg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
