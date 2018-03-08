8891225a4143dfd8f2c9a16ab5c172ae7d843cff
Sam Welch <shwelch@stu.naperville203.org>
2018-03-08 12:07:57 -0600

    hi
    hey

--- Tracking Buses SE/Tracking Buses SE/ViewController.swift
+++ Tracking Buses SE/Tracking Buses SE/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var htmlload: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let htmlpath = Bundle.main.path(forResource: "TrackingBusesTake1", ofType: "html")
+        let url = URL(fileURLWithPath: htmlpath!)
+        let request = URLRequest(url: url)
+        htmlload.load(request)
+        
     }
 
     override func didReceiveMemoryWarning() {
